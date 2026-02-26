d00b128dcefab7f42c11b74496c5f6132bfb6adf
isv-bit <isvdisenointeractivo@gmail.com>
2026-02-26 13:26:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st 
+from PIL import Image 
 st.title("Mi primera app")
 st.header("Mucho gusto, Isabella.")
+
+image = Image.open("Nala.JPEG")
+st.image(image, caption = "Esta es mi mascota")
+texto = st.text_input ("Imagen texto"," texto inicial")
+st.write("El texto que has escrito es", texto)
