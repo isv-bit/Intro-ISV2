03c56cf6b72f4cd2eba6e9733027942c973bace2
isv-bit <isvdisenointeractivo@gmail.com>
2026-02-26 13:33:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ st.header("Mucho gusto, Isabella.")
 image = Image.open("Nala.jpeg")
 st.image(image, caption = "Esta es mi mascota")
 texto = st.text_input ("Imagen texto"," texto inicial")
-  st.write("El texto que has escrito es", texto)
-st.button("presiona el botón"):
+st.write("El texto que has escrito es", texto)
+if st.button("presiona el botón"):
   st.write("has presionado")
